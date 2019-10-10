13e55da7a8f639e16369b860f428616f1887ecbd
William Siqueira <wscesar@gmail.com>
2019-10-10 13:35:27 -0300

    added restaurant name to product list

--- src/app/product-list/product-list.page.ts
+++ src/app/product-list/product-list.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductService } from '../services/product.service';
+import { RestaurantService } from '../services/restaurant.service';
+import { Restaurant } from '../model/restaurant.model';
 
 @Component({
   selector: 'app-product-list',
@@ -9,16 +11,27 @@ import { ProductService } from '../services/product.service';
 export class ProductListPage implements OnInit {
 
     private products: Product[];
+    private restaurants: Restaurant[] = [];
     private isLoading = true;
 
     constructor(
+        private restaurantService: RestaurantService,
         private productService: ProductService
     ) {}
 
     ngOnInit() {
         this.productService.getAllProducts().subscribe(products => {
-            this.isLoading = false;
+
             this.products = products;
+
+            for (const p of products) {
+                this.restaurantService.getRestaurant(p.restaurantId).subscribe(restaurant => {
+                    this.restaurants.push(restaurant);
+                    this.isLoading = false;
+                    // console.log(restaurant);
+                });
+            }
+
         });
     }
 
